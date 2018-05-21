8131f80ce2ce29a00920c8b119daed08d4ca8b86
Nikiya Simpson <nsimpson@uams.edu>
2018-05-21 13:57:43 -0500

    create list to hold hards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 /*
- * Create a list that holds all of your cards
- */
-
-
+ * Create a list that holds all of your cards*/
+let cardList = document.querySelectorAll("i");
+console.log(cardList);
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -27,9 +26,9 @@ function shuffle(array) {
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ 
+ 
+
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
@@ -37,8 +36,59 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 /* Turn Card Over */
- let selectedCard = $("li.card");
-selectedCard.click(function(){
-    $(this).addClass( "open show" );
-});
+
+let openCards = [];
+let selectedCard = $("li.card");
+ /* set up the event listener for a card. If a card is clicked:*/
+    selectedCard.click(showCard);
+
+    /*  - display the card's symbol (put this functionality in another function that you call from this one)*/
+    /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) */
+
+function showCard(){
+        if($(this).classlist !== 'card open show match'){
+        $(this).toggleClass('open');
+        $(this).toggleClass('show');
+        var card = $(this).querySelector("i");
+        openCards.push(card);
+        /*cardList.push($(this));*/
+        console.log(openCards);
+        if(openCards.length === 2){
+                matchCard();
+        }
+        console.log(cardList);
+}
+}
+
+
+
+function openCard(card){
+
+
+        card.addClass( "open show" );
+        openCards.push(card);
+        /*openCards["IsOpen"] = "true"; // assign property of an // Object via bracket syntax
+        openCards["picture"] = pic;*/
+        
+}
+
+
+function closeCard(card){
+    card.removeClass( "open show" );
+}
+
+function matchCard() {
+    if (openCards[0] === openCards[1]) {
+        console.log("We've got a match");
+        openCards = [];
+        cardList[cardList.length-1].addClass("match");
+        cardList[cardList.length-2].addClass("match");
+    } 
+    else {
+        console.log("Not a match");
+       	cardList.slice(-2,2).removeClass("open show");
+        openCards = [];
+    }
+}
+
 
